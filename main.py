cb33c7d81aac7348ad8f4cbcb84be6dccbdf43f9
songdongho4736-netizen <songdongho4736@gmail.com>
2026-03-18 21:33:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random as rd
 import streamlit as st
 
-st.title("멈추기 게")
+st.title("멈추기 게임")
 
 if st.button("game start"):
   num = rd.random()
-  if num < 0.1:
+  if num < 0.3:
     st.error("멈춰!")
   else:
     st.success("통과!")
