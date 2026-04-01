18e150c01deef8043729bf262ccb70abfa12116f
songdongho4736-netizen <songdongho4736@gmail.com>
2026-04-01 21:57:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,9 +36,11 @@ st.markdown("<h1 style='text-align: center; color: white;'>멈추기게임</h1>"
 col1,col2,col3 = st.columns([1,2,1])
 
 with col2:
-  with open("background.jpg","rb") as f:
-    img_data = base64.b64encode(f.read()).decode() 
-  if st.button(f'<ing src="data:image/jpeg;base64,{img_data}" width="300">',use_container_with=True):
+ # with open("background.jpg","rb") as f:
+  #  img_data = base64.b64encode(f.read()).decode()
+   st.image("background.jpg",use_container_with=True) 
+ # if st.button(f'<ing src="data:image/jpeg;base64,{img_data}" width="300">',use_container_with=True):
+    if st.button("게임시작")
     num = rd.random()
     if num < 0.3:
       st.error("멈춰!")
