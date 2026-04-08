b20e5edbfc1d9445e4d463a22a4017e54cc1b3ad
songdongho4736-netizen <songdongho4736@gmail.com>
2026-04-08 22:00:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def set_background(main_bg):
       button[kind="secondary"] {{
             background-image: url(data:image/jpeg;base64,{b64_sponge});
             background-size: cover;
+            width: 300px;
             
       }}
       </style>
